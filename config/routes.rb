622cae2e852707ae652ad64ddae2ddad26ee50ed
Marthyn <Marthyn@live.nl>
2016-03-02 22:07:50 +0100

    Fix registrationscontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :categories, except: [:show]
   resources :expenses
   resources :shops, except: [:show]
