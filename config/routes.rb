7a3a62004e77b44544ebed9f7105e6239dd18b3b
Marthyn <Marthyn@live.nl>
2016-03-02 22:11:07 +0100

    Use users controller for sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :registrations => "user/registrations" }
+  devise_for :users, :controllers => { :registrations => "users/registrations" }
   resources :categories, except: [:show]
   resources :expenses
   resources :shops, except: [:show]
