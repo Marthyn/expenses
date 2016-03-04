d8bb087a5e3d911b94b38377f51a74a419447247
Marthyn <Marthyn@live.nl>
2016-03-04 10:56:46 +0100

    Fix migration

--- db/migrate/20160304094830_add_counters.rb
+++ db/migrate/20160304094830_add_counters.rb
@@ -2,15 +2,5 @@ class AddCounters < ActiveRecord::Migration
   def change
     add_column :shops, :expenses_count, :integer, default: 0
     add_column :categories, :expenses_count, :integer, default: 0
-
-    Account.pluck(:name).each do |a|
-      Apartment::Tenant.switch(a)
-      Shop.all.each do |c|
-        c.update_attribute(:expenses_count, c.expenses.count)
-      end
-      Category.all.each do |c|
-        c.update_attribute(:expenses_count, c.expenses.count)
-      end
-    end
   end
 end
